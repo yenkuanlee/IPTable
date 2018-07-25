d2d5dd5ec91b1799a69a6083e3af407bc12c00fb
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-07-25 04:31:31 +0000

    update object node

--- ObjectNode.py
+++ ObjectNode.py
@@ -9,6 +9,7 @@ class ObjectNode:
     def __init__(self):
         self.api = ipfsapi.connect(IPFS_IP,IPFS_PORT)
         self.ObjectHash = "NULL"
+        self.ObjectMatch = dict()
 
     def new(self, UserID):
         Tbyte = bytes(json.dumps({"Data":UserID}))
@@ -18,17 +19,49 @@ class ObjectNode:
     def load(self, Fhash):
         self.ObjectHash = Fhash
 
+    def match(self, ObjectName):
+        Tbyte = bytes(json.dumps({"Data":ObjectName}))
+        Thash = self.api.object_put(io.BytesIO(Tbyte))['Hash']
+        self.api.pin_add(Thash)
+        self.ObjectMatch[ObjectName] = Thash
+        self.ObjectMatch[Thash] = ObjectName
+
     def GetObjectInfo(self):
-        return self.api.object_get(self.ObjectHash)
+        Tdict = self.api.object_get(self.ObjectHash)
+        return Tdict
+
+    def GetInfo(self):
+        Tdict = self.api.object_get(self.ObjectHash)
+        Rdict = dict()
+        Rdict["Data"] = Tdict["Data"]
+        for x in Tdict["Links"]:
+            kv = json.loads(x["Name"])
+            for y in kv:
+                Rdict[y] = kv[y]
+        return Rdict
 
     def AddHash(self,ObjectName,Fhash):
         self.ObjectHash = self.api.object_patch_add_link(self.ObjectHash,ObjectName,Fhash)['Hash']
         self.api.pin_add(self.ObjectHash)
 
+    def RemoveHash(self,ObjectName):
+        OldOhash = self.ObjectHash
+        self.ObjectHash = self.api.object_patch_rm_link(self.ObjectHash,ObjectName)['Hash']
+        self.api.pin_add(self.ObjectHash)
+        self.api.pin_rm(OldOhash)
+        self.api.repo_gc()
 
-#Jres = api.object_get("QmQQa3HUgDtXzwwBcdcwsSVPWWJ7PVWc8UmbADV1FhxB8n")
-#Jres = json.loads(res)
-#data = Jres['Data']
-#Links = Jres['Links']
+    def AddField(self,field,value):
+        if field not in self.ObjectMatch:
+            self.match(field)
+        self.AddHash(json.dumps({field:value}),self.ObjectMatch[field])
 
-#print(Links)
+    def RemoveField(self,field):
+        if field not in self.ObjectMatch:
+            return "FIELD NOE EXIST."
+        Tdict = self.api.object_get(self.ObjectHash)
+        for x in Tdict["Links"]:
+            kv = json.loads(x["Name"])
+            if field in kv.keys():
+                aa = (json.dumps({field:kv[field]}))
+                self.RemoveHash(aa)
