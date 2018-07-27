50881100587dd0603fec0915237b0436e2a65097
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-07-27 08:41:07 +0000

    enable sql update function

--- ipfs_tablefdw.py
+++ ipfs_tablefdw.py
@@ -11,10 +11,11 @@ class IPFSFdw(ForeignDataWrapper):
     def __init__(self, fdw_options, fdw_columns):
         super(IPFSFdw, self).__init__(fdw_options, fdw_columns)
         self.Dflag = False
-        self.Dlist = list()
-        self.randomid = fdw_options["randomid"]
+        self.Dlist = list() # delete
+        self.Ulist = list() # update
+        self.table_name = fdw_options["table_name"]
         CleanNode = ObjectNode.ObjectNode()
-        CleanNode.new(self.randomid)
+        CleanNode.new(self.table_name)
         self.fhash = fdw_options.get('fhash', CleanNode.ObjectHash)
         self.columns = fdw_columns
         self.tx_hook = fdw_options.get('tx_hook', False)
@@ -22,17 +23,17 @@ class IPFSFdw(ForeignDataWrapper):
         self.api = ipfsapi.connect('127.0.0.1', 5001)
         self.conn = psycopg2.connect(database="postgres",user="postgres",host="127.0.0.1", port="5432")
         self.cur = self.conn.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS _lookup(randomid text PRIMARY KEY, fhash text);")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS _lookup(table_name text PRIMARY KEY, fhash text);")
         self.conn.commit()
-        self.cur.execute("SELECT FHASH FROM _lookup WHERE randomid = '"+self.randomid+"';")
+        self.cur.execute("SELECT FHASH FROM _lookup WHERE table_name = '"+self.table_name+"';")
         try:
             DontCare = self.cur.fetchone()[0]
         except:
-            self.cur.execute("INSERT INTO _lookup VALUES('"+self.randomid+"','"+self.fhash+"');")
+            self.cur.execute("INSERT INTO _lookup VALUES('"+self.table_name+"','"+self.fhash+"');")
             self.conn.commit()
 
     def execute(self, quals, columns):
-        self.cur.execute("SELECT FHASH FROM _lookup WHERE randomid = '"+self.randomid+"';")
+        self.cur.execute("SELECT FHASH FROM _lookup WHERE table_name = '"+self.table_name+"';")
         self.fhash = self.cur.fetchone()[0]
         Jres = self.api.object_get(self.fhash)
         for x in Jres['Links']:
@@ -52,10 +53,21 @@ class IPFSFdw(ForeignDataWrapper):
             for x in self.Dlist:
                 table.RemoveHash(x)
             self.fhash = table.ObjectHash
-            self.cur.execute("UPDATE _lookup SET fhash = '"+self.fhash+"' WHERE randomid = '"+self.randomid+"';")
+            self.cur.execute("UPDATE _lookup SET fhash = '"+self.fhash+"' WHERE table_name = '"+self.table_name+"';")
             self.conn.commit()
+            # For Update
+            for x in self.Ulist:
+                a = ObjectNode.ObjectNode()
+                a.new("cat")
+                a.AddRow(x)
+                table.AddHash(x['tsid'],a.ObjectHash)
+            self.fhash = table.ObjectHash
+            self.cur.execute("UPDATE _lookup SET fhash = '"+self.fhash+"' WHERE table_name = '"+self.table_name+"';")
+            self.conn.commit()
+            self.Ulist = list()
             self.Dlist = list()
             self.Dflag = False
+            
 
     def insert(self, values):
         table = ObjectNode.ObjectNode()
@@ -63,17 +75,20 @@ class IPFSFdw(ForeignDataWrapper):
         a = ObjectNode.ObjectNode()
         a.new("dog")
         a.AddRow(values)
-        Tlist = table.GetObjectInfo()["Links"]
+        #Tlist = table.GetObjectInfo()["Links"]
         ts = str(int(time.time()*1000))
         table.AddHash(ts,a.ObjectHash)
         self.fhash = table.ObjectHash
-        self.cur.execute("UPDATE _lookup SET fhash = '"+self.fhash+"' WHERE randomid = '"+self.randomid+"';")
+        self.cur.execute("UPDATE _lookup SET fhash = '"+self.fhash+"' WHERE table_name = '"+self.table_name+"';")
         self.conn.commit()
         self.Dflag = False
 
     def delete(self, oldvalues):
         self.Dlist.append(oldvalues)
 
+    def update(self, oldvalues, newvalues):
+        self.Dlist.append(oldvalues)
+        self.Ulist.append(newvalues)
 
     @property
     def rowid_column(self):
