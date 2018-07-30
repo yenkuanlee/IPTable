39d1c8d9f4e6b31302ec2262d4047148cc8bbd5f
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-07-30 02:34:14 +0000

    deploy init

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+sudo /etc/init.d/postgresql restart
+sudo -u postgres psql -c "CREATE EXTENSION MULTICORN;"
+sudo -u postgres psql -c "CREATE SERVER ipserver foreign data wrapper multicorn options (wrapper 'multicorn.ipfs_tablefdw.IPFSFdw');"
+ipfs init
+ipfs daemon &
