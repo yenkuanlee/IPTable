706e8eb515ee3b357549b0a1c0120704c3876828
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-09-12 09:52:14 +0000

    IOTABLE

--- IOTAable/IOTATransaction.py
+++ IOTAable/IOTATransaction.py
@@ -1,3 +1,4 @@
+import base64
 import iota
 from iota import TryteString
 import json
@@ -37,16 +38,34 @@ class IOTATransaction:
                 TryteStringMessage = str(Vtxn['signature_message_fragment'])
                 return TryteString(str.encode(TryteStringMessage)).decode()
 
+    def Kencode(self,key, clear):
+        enc = []
+        for i in range(len(clear)):
+            key_c = key[i % len(key)]
+            enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+            enc.append(enc_c)
+        return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+    def Kdecode(self,key, enc):
+        dec = []
+        enc = base64.urlsafe_b64decode(enc).decode()
+        for i in range(len(enc)):
+            key_c = key[i % len(key)]
+            dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+            dec.append(dec_c)
+        return "".join(dec)
+
     def GetFhash(self, table_name):
         conn = psycopg2.connect(database="postgres",user="postgres",host="127.0.0.1", port="5432")
         cur = conn.cursor()
         cur.execute("SELECT FHASH FROM _lookup WHERE table_name = '"+table_name+"';")
+        a = self.Kencode('KEVIN',cur.fetchone()[0])
         try:
-            return cur.fetchone()[0]
-        except:
-            return "ERROR"
+            return self.Kencode('KEVIN',cur.fetchone()[0])
+        except Exception as e:
+            return {"status":"ERROR", "log":str(e)}
     def CreateTable(self,table_name, TID):
         try:
+            fhash = self.Kdecode('KEVIN',Jinfo['fhash'])
             Jinfo = json.loads(self.GetTransactionMessage(TID))
             sql = "CREATE FOREIGN TABLE "+table_name+Jinfo['table_schema']+"SERVER ipserver OPTIONS (table_name '"+table_name+"', fhash '"+Jinfo['fhash']+"');"
             conn = psycopg2.connect(database="postgres",user="postgres",host="127.0.0.1", port="5432")
--- IOTAable/test.py
+++ IOTAable/test.py
@@ -8,18 +8,18 @@ Ftag = 'IPTABLETEST'
 api = ipfsapi.connect('127.0.0.1', 5001)
 
 To = 'BXOM9LUNLPSEXBRJV9UUNLHSUHABEOGHQOGNBNBUEYSGOFZOEPYKEYRSFTXBOEJLUODUQXXGQ9NWQBSGH'
+TargerPeerID = 'KEVIN'
 
 table_info = dict()
 table_info['table_name'] = 'accounting'
 table_info['table_schema'] = '(TSID bigint,Adate text,item text,description text,twd int)'
 table_info['description'] = '這是Kevin的記帳'
 table_info['fhash'] = a.GetFhash('accounting')
-table_info['table_rowCount'] = len(api.object_get(table_info['fhash'])['Links'])
+table_info['table_rowCount'] = 12#len(api.object_get(table_info['fhash'])['Links'])
 
-#pt = a.MakePreparingTransaction(To, json.dumps(table_info), tag=Ftag)
-#a.SendTransaction([pt])
-#print(a.GetTransactionHash())
+pt = a.MakePreparingTransaction(To, json.dumps(table_info), tag=Ftag)
+a.SendTransaction([pt])
+print(a.GetTransactionHash())
 
-TID = 'CZIIPIYJNSYZCFYXXHYJHXLPMDYUIVLROQOKEEVFHZLZJVLZBPGAIHBBVENQCBAIGPFGGAKXVUO9Z9999'
-#info = a.GetTransactionMessage(TID)
-print(a.CreateTable('kevin',TID))
+#TID = 'GLKGBQOOHIUAJIYEPAWFNRTVYZIMIQYIUNPRNLSSJZTTIRSQVMARYQFJPESXXGHMYWBWHFITRQCAA9999'
+#print(a.CreateTable('kevin',TID))
